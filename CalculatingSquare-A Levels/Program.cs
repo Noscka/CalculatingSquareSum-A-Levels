0740124f4558249eaf58bd000e79f01fb5b8b37c
Adam Krawczyk <Adam882200@gmail.com>
2022-10-12 09:34:21 +0100

    Add project files.

---
+++ CalculatingSquare-A Levels/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CalculatingSquare_A_Levels
+{
+	static internal class Program
+	{
+		static int SumRange(int startPos, int Range)
+		{
+			int output = 0;
+			for(int i = 0; i < Range; i++)
+			{
+				output += i+startPos;
+			}
+			return output;
+		}
+
+		static int SumRows(int ColumnCount, int RowCount)
+		{
+			int output = 0;
+			for (int i = 1; i < RowCount+1; i++)
+			{
+				output += SumRange(i, ColumnCount);
+			}
+			return output;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Full Output: {SumRows(10, 10)}");
+		}
+	}
+}
